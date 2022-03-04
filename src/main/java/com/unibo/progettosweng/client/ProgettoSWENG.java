98fa8c307b254c5adfa12d4edd3a014b111f2e74
Sonia <sonia.nicoletti@studio.unibo.it>
2022-03-04 23:35:33 +0100

    Aggiunti elementi alla homepage

--- src/main/java/com/unibo/progettosweng/client/ProgettoSWENG.java
+++ src/main/java/com/unibo/progettosweng/client/ProgettoSWENG.java
@@ -38,7 +38,34 @@ public class ProgettoSWENG implements EntryPoint {
    * This is the entry point method.
    */
   public void onModuleLoad() {
-    final Button sendButton = new Button("Send");
+
+    // Informazioni universita'
+    final String uniInfoString = "Lorem ipsum dolor sit amet, " +
+            "consectetur adipiscing elit, sed do eiusmod tempor incididunt" +
+            " ut labore et dolore magna aliqua. Ut enim ad minim veniam, " +
+            "quis nostrud exercitation ullamco laboris nisi ut aliquip ex " +
+            "ea commodo consequat. Duis aute irure dolor in reprehenderit in " +
+            "voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+            "Excepteur sint occaecat cupidatat non proident, sunt in culpa " +
+            "qui officia deserunt mollit anim id est laborum.";
+    final HTML uniInfo = new HTML(uniInfoString);
+    RootPanel.get("universitaInfo").add(uniInfo);
+
+    // Informazioni dipartimenti
+    final String dipInfoString = "Qui ci sono i dipartimenti";
+    final HTML dipInfo = new HTML(dipInfoString);
+    RootPanel.get("dipInfo").add(dipInfo);
+
+    // Contatti
+    final String contattiString = "Telefono: 123-456-789 <br /> Indirizzo: Via delle Stelle, 56 Bologna <br /> E-mail: uniscientia@mail.com";
+    final HTML contatti = new HTML(contattiString);
+    RootPanel.get("contattiInfo").add(contatti);
+
+    // Pulsante log in
+    final Button login = new Button("Il mio portale");
+    RootPanel.get("login").add(login);
+
+/*    final Button sendButton = new Button("Send");
     final TextBox nameField = new TextBox();
     nameField.setText("GWT User");
     final Label errorLabel = new Label();
@@ -86,25 +113,25 @@ public class ProgettoSWENG implements EntryPoint {
 
     // Create a handler for the sendButton and nameField
     class MyHandler implements ClickHandler, KeyUpHandler {
-      /**
+      *//**
        * Fired when the user clicks on the sendButton.
-       */
+       *//*
       public void onClick(ClickEvent event) {
         sendNameToServer();
       }
 
-      /**
+      *//**
        * Fired when the user types in the nameField.
-       */
+       *//*
       public void onKeyUp(KeyUpEvent event) {
         if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
           sendNameToServer();
         }
       }
 
-      /**
+      *//**
        * Send the name from the nameField to the server and wait for a response.
-       */
+       *//*
       private void sendNameToServer() {
         // First, we validate the input.
         errorLabel.setText("");
@@ -142,6 +169,6 @@ public class ProgettoSWENG implements EntryPoint {
     // Add a handler to send the name to the server
     MyHandler handler = new MyHandler();
     sendButton.addClickHandler(handler);
-    nameField.addKeyUpHandler(handler);
+    nameField.addKeyUpHandler(handler);*/
   }
 }
