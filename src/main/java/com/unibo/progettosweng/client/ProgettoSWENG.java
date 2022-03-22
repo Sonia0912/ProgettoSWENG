44d17ab97ef5974747dc4d8822ec91d9134dfdbc
ErikaColonna <colonna.erika@gmail.com>
2022-03-22 18:42:59 +0100

    inserito time picker

--- src/main/java/com/unibo/progettosweng/client/ProgettoSWENG.java
+++ src/main/java/com/unibo/progettosweng/client/ProgettoSWENG.java
@@ -48,8 +48,11 @@ public class ProgettoSWENG implements EntryPoint {
     RootPanel.get("pannelloMenu").add(hPanel);
 
     // Di default mostro il contenuto della homepage
-    Homepage hp = new Homepage();
-    hp.aggiungiContenuto();
+//    Homepage hp = new Homepage();
+//    hp.aggiungiContenuto();
+
+    InserimentoEsame ie = new InserimentoEsame();
+    ie.aggiungiContenuto();
 
 
     // Se clicco su Home
