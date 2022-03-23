6ab1d7f1c917cded58bf6b2042a44281a34ff03a
Gian207 <gianluca.gueli@studio.unibo.it>
2022-03-23 09:22:31 +0100

    versione corretta dell'implementazione DB

---
+++ src/main/java/com/unibo/progettosweng/client/model/Utente.java
@@ -0,0 +1,61 @@
+package com.unibo.progettosweng.client.model;
+
+
+import java.io.Serializable;
+
+
+public class Utente implements Serializable {
+
+
+    private static final long serialVersionUID = 6920472853042767550L;
+    private String nome;
+    private String cognome;
+    private String username;
+    private String password;
+    private String tipo;
+
+    public Utente(){
+
+    }
+
+    public Utente(String nome, String cognome, String username, String password, String tipo ){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.username = username;
+        this.password = password;
+        this.tipo = tipo;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+    public String getCognome(){
+        return this.cognome;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+    public String getPassword(){return password;}
+    public String getTipo(){return tipo;}
+
+    public void setNome(String nome){
+        this.nome = nome;
+
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+
+    }
+    @Override
+    public String toString(){
+        return this.nome+" "+this.cognome+" "+this.username+" "+this.tipo;
+    }
+
+
+}
---
+++ src/main/java/com/unibo/progettosweng/server/UtenteServiceImpl.java
@@ -0,0 +1,130 @@
+package com.unibo.progettosweng.server;
+
+import com.unibo.progettosweng.client.UtenteService;
+import com.unibo.progettosweng.client.model.Serializer.SerializerUtente;
+import com.unibo.progettosweng.client.model.Utente;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+import org.mapdb.Serializer;
+
+import javax.servlet.ServletContext;
+
+/**
+ * The server-side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class UtenteServiceImpl extends RemoteServiceServlet implements UtenteService {
+
+    DB db;
+    HTreeMap<String,Utente> map;
+
+    private void createOrOpenDB(){
+        this.db = getDb("utenti.db");
+        this.map = this.db.hashMap("utentiMap").counterEnable().keySerializer(Serializer.STRING)
+                .valueSerializer(new SerializerUtente()).createOrOpen();
+    }
+
+    @Override
+    public String add(String[] input) throws IllegalArgumentException {
+        createOrOpenDB();
+        for(int i = 0; i < input.length; i++) {
+            input[i] = escapeHtml(input[i]);
+        }
+        Utente ut = new Utente(input[0], input[1], input[2], input[3], input[4]);
+        if(controlloUtenteDuplicato( map,ut)){
+            return "Utente già inserito!";
+        }else {
+            map.put(String.valueOf(map.size() + 1), ut);
+            db.commit();
+            return "(size: " + map.size()+ ") L'utente " + ut.getNome() + " " + ut.getCognome() + " è stato creato e aggiunto al database.";
+        }
+    }
+
+    @Override
+    public String remove(String username) throws IllegalArgumentException {
+        createOrOpenDB();
+        for ( String i: map.getKeys()) {
+            if(map.get(i).getUsername().equals(username)){
+                map.remove(i);
+                db.commit();
+                return "La taglia: " + map.size() + " L'utente " + username + " è  stato rimosso.";
+            }
+        }
+        return "Nessun utente Presente!";
+    }
+
+    @Override
+    public Utente[] getUtenti() throws Exception{
+        createOrOpenDB();
+        Utente[] utenti = new Utente[map.size()];
+
+        int k = 0;
+        for ( String i: map.getKeys()) {
+            utenti[k] = map.get(i);
+            k++;
+        }
+        return utenti;
+    }
+
+    @Override
+    public Boolean login(String username, String password) throws Exception {
+        createOrOpenDB();
+        for (String i : map.getKeys()) {
+            if(map.get(i).getUsername().equals(username) && map.get(i).getPassword().equals(password)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+    //metodo che viene invocato quando vengono modificato le informazioni degli utenti
+    @Override
+    public void aggiorna(Utente ut){
+        createOrOpenDB();
+        for ( String i: map.getKeys()) {
+            if(map.get(i).getUsername().equals(ut.getUsername())){
+                map.replace(i,ut);
+            }
+        }
+
+    }
+
+
+    private boolean controlloUtenteDuplicato(HTreeMap<String,Utente> map, Utente utente){
+        for ( String i: map.getKeys()) {
+            if (map.get(i).getUsername().equals(utente.getUsername()) ){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // metodo per assicurarsi che non le stringhe vengano lette come tali e non come codice html
+    private String escapeHtml(String html) {
+        if (html == null) {
+            return null;
+        }
+        return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(
+                ">", "&gt;");
+    }
+
+
+    private DB getDb(String nameDB){
+        ServletContext context = this.getServletContext();
+        synchronized (context) {
+            DB db = (DB)context.getAttribute("DB");
+            if(db == null) {
+                db = DBMaker.fileDB(nameDB).make();
+                context.setAttribute("DB", db);
+            }
+            return db;
+        }
+    }
+
+}
