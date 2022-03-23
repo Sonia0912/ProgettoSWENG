6bf6b0a7c0fc61e93865e61e03e640188b9b2b76
Gian207 <gianluca.gueli@studio.unibo.it>
2022-03-23 09:57:49 +0100

    aggiustamento branch develop alla nuova implementazione DB

---
+++ src/main/java/com/unibo/progettosweng/client/model/Esame.java
@@ -0,0 +1,59 @@
+package com.unibo.progettosweng.client.model;
+
+public class Esame {
+
+    private String data;
+    private String ora;
+    private String difficolta;
+    private String aula;
+    private String nomeCorso;
+
+    public Esame(String data, String ora, String difficolta, String aula, String nomeCorso) {
+        super();
+        this.data = data;
+        this.ora = ora;
+        this.difficolta = difficolta;
+        this.aula = aula;
+        this.nomeCorso = nomeCorso;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getAula() {
+        return aula;
+    }
+
+    public String getDifficolta() {
+        return difficolta;
+    }
+
+    public String getNomeCorso() {
+        return nomeCorso;
+    }
+
+    public String getOra() {
+        return ora;
+    }
+
+    public void setAula(String aula) {
+        this.aula = aula;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void setDifficolta(String difficolta) {
+        this.difficolta = difficolta;
+    }
+
+    public void setNomeCorso(String nomeCorso) {
+        this.nomeCorso = nomeCorso;
+    }
+
+    public void setOra(String ora) {
+        this.ora = ora;
+    }
+}
